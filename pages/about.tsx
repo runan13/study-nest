c65fea7d71b889d8e85d08ab828bd72ef9c1ce62
runan13 <tjrrhj123@gmail.com>
2022-08-07 11:36:35 +0900

    Static Pre Rendering

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
+// Static Pre Rendering이 이루어지기 때문에
+// browser의 자바스크립트가 비활성화 되어있거나 네트워크가 지연되어도 초기의 state값 및 component는 브라우저에 출력되어 보여진다
 export const About: NextPage = () => {
-  return <div>about</div>
+  const [count, setCount] = useState<number>(0)
+
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={()=>setCount(prev => prev+1)}>+</button>
+    </div>
+  )
 }
 
 // React component가 default로 export 되어야 한다.
