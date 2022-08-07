450273f84705f510ebd033ba3a1bb5f4f5829c3b
runan13 <tjrrhj123@gmail.com>
2022-08-07 12:19:11 +0900

    Custom app

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import NavBar from '../components/NavBar'
 
 // Static Pre Rendering이 이루어지기 때문에
 // browser의 자바스크립트가 비활성화 되어있거나 네트워크가 지연되어도 초기의 state값 및 component는 브라우저에 출력되어 보여진다
@@ -9,7 +8,6 @@ const About: NextPage = () => {
 
   return (
     <div>
-      <NavBar />
       <h1>Count : {count}</h1>
       <button onClick={() => setCount(prev => prev + 1)}>+</button>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from 'next'
-import NavBar from '../components/NavBar'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <NavBar />
       <h1>Home</h1>
     </div>
   )
