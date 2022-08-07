7fe7c543a0a504310b53a8c887f52e4b76a09764
runan13 <tjrrhj123@gmail.com>
2022-08-07 12:08:13 +0900

    CSS Module

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css"
 
 // Routing
 const NavBar: NextPage = () => {
@@ -9,10 +10,10 @@ const NavBar: NextPage = () => {
     return (
         <nav>
             <Link href="/">
-                <a style={{color: router.pathname === "/" ? "red": "blue"}}>Link</a>
+                <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
             </Link>
             <Link href="/about">
-                <a style={{color: router.pathname === "/about" ? "red": "blue"}}>Link</a>
+                <a className={router.pathname === "/about" ? styles.active : ""}>About</a>
             </Link>
         </nav>
     )
